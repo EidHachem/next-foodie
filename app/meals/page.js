b12fb5faf1d7c7efaaf046b94c50c4cfc97d16f4
eid <eidhachem1@gmail.com>
2024-09-24 19:09:05 +0300

    ADd meals page styles and data

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,7 +1,25 @@
-export default function MealPage() {
+import Link from "next/link"
+import classes from "./page.module.css"
+import MealsGrid from "@/components/meals/meals-grid"
+
+export default function MealsPage() {
   return (
-    <main>
-      <h1>Meals</h1>
-    </main>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose you favourite recipe and cook it yourself. it is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share your favourite recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
   )
 }
